12af15ee27786e9784f628e7ad66838f56104469
Yunior Alayo <yunior.alayo@gmail.com>
2024-08-23 13:13:35 +0200

    Refactor to fix the post request and getting the generated pdf.

--- components/web/src/app/web/core.clj
+++ components/web/src/app/web/core.clj
@@ -1,28 +1,36 @@
 (ns app.web.core
   (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.params :as params]
             [app.html.interface :refer [index-page]]
             [app.invoicepdf.interface :refer [create-invoice]]))
 
-(defn generate-invoice-handler [request]
-  (let [invoice-data (-> request :form-params)
-        pdf-path "output/invoice.pdf"]
-    (create-invoice invoice-data pdf-path)
-;; Return response with a link to download the PDF
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body "<a href='/download/invoice.pdf'>Download Invoice</a>"}))
+(def generate-invoice-handler
+  {:name ::post
+   :enter (fn [context]
+            (let [invoice-data (-> context :request :params)
+                  pdf-path "output/invoice.pdf"]
+              (create-invoice invoice-data pdf-path)
+              (assoc context :response {:status 200
+                                        :headers {"Content-Type" "text/html"}
+                                        :body "<a href='/download/invoice.pdf'>Download Invoice</a>"})))})
 
 (def routes
   #{["/" :get (fn [_] {:status 200 
                        :headers {"Content-Type" "text/html" "Content-Security-Policy" "img-src 'self'"} 
                        :body (index-page)}) :route-name ::index-page]
-    ["/generate-invoice" :post generate-invoice-handler :route-name ::generate-invoice]
+    ["/generate-invoice" :post [(body-params/body-params)
+                                params/keyword-params
+                                generate-invoice-handler] 
+     :route-name ::generate-invoice]
     ["/download/:file" :get (fn [{:keys [path-params]}]
+                              (println path-params)
                               (let [file-path (str "output/" (:file path-params))]
                                 (if (.exists (java.io.File. file-path))
                                   {:status 200
                                    :headers {"Content-Type" "application/pdf"}
-                                   :body (java.nio.file.Files/readAllBytes (java.nio.file.Paths/get file-path))}
+                                   :body (java.nio.file.Files/readAllBytes
+                                           (java.nio.file.Paths/get file-path (into-array String [])))}
                                   {:status 404 :body "File not found"})))
 		 :route-name ::download-file]})
 
