ee0959db269883581bfef0c6bd5fa9dfd165b727
Yunior Alayo <yunior.alayo@gmail.com>
2024-08-28 10:31:11 +0200

    Added method to expose the datasource.

--- components/user/src/app/user/interface.clj
+++ components/user/src/app/user/interface.clj
@@ -1,5 +1,9 @@
 (ns app.user.interface
-  (:require [app.user.core :as core]))
+  (:require [app.user.core :as core]
+            [app.user.database :as db]))
 
 (defn get-routes []
   core/routes)
+
+(defn get-datasource []
+  db/ds)
