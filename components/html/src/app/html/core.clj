bf94393541c8af36ea5d0bca63b1e2003189b6b6
Yunior Alayo <yunior.alayo@gmail.com>
2024-09-08 13:52:46 +0200

    Added a variable to pass the title.

--- components/html/src/app/html/core.clj
+++ components/html/src/app/html/core.clj
@@ -6,10 +6,10 @@
             [app.html.dashboard :as dashboard]))
 
 ;; Prepare the hicup to return it as html
-(defn template [html-body]
+(defn template [html-body title]
   [:html
    [:head
-    [:title "Title"]
+    [:title title]
     [:link {:href "tailwind.min.css" :rel "stylesheet"}]
     [:script {:src "htmx.min.js"}]]
    [:body (h/raw html-body)]])
@@ -21,21 +21,20 @@
              (h/html)
              (str))})
 
-(defn respond [content]
-  (ok (template (str (h/html (content))))))
+(defn respond [content title]
+  (ok (template (str (h/html (content))) title)))
 
-(defn respond-with-params [content value]
-  (ok (template (str (h/html (content value))))))
+(defn respond-with-params [content value title]
+  (ok (template (str (h/html (content value))) title)))
 
 (defn index-page-handler [context]
-  (respond index/index-page))
+  (respond index/index-page "Herzlich Willkommen!"))
 
 (defn dashboard-handler [context]
-  (println "TEST: " (-> context :session))
   (let [session (-> context :session)]
     (if (empty? session)
       (response/redirect "/sign-in")
-      (respond-with-params dashboard/content {:email (:email session) :created-at (:created-at session)}))))
+      (respond-with-params dashboard/content {:email (:email session) :created-at (:created-at session)} "Dashboard"))))
 
 (def routes
   #{["/" :get index-page-handler :route-name ::index-page]
