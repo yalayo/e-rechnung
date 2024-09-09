9583631eac89a1c368cacb05da63bf34e4f40ef1
Yunior Alayo <yunior.alayo@gmail.com>
2024-09-10 00:15:19 +0200

    Show button if the product is not selected.

--- components/html/src/app/html/product.clj
+++ components/html/src/app/html/product.clj
@@ -32,23 +32,35 @@
    [:div {:class "mt-5 flex lg:ml-4 lg:mt-0"} 
     [:span
      {:class "sm:ml-3"}
-     [:button
-      {:type "button",
-       :class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-       :hx-get (str "/products/" (:article-id product)) 
-       :hx-target "this"
-       :hx-swap "outerHTML"}
-      [:svg
-       {:class "-ml-0.5 mr-1.5 h-5 w-5",
-        :viewBox "0 0 20 20",
-        :fill "currentColor",
-        :aria-hidden "true"}
-       [:path
-        {:fill-rule "evenodd",
-         :d
-         "M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z",
-         :clip-rule "evenodd"}]]
-      "Auswählen"]]
+     (if (:selected product)
+       [:div {:class "flex justify-center items-center h-9"}
+        [:svg
+         {:class "-ml-0.5 mr-1.5 h-5 w-5",
+          :viewBox "0 0 20 20",
+          :fill "currentColor",
+          :aria-hidden "true"}
+         [:path
+          {:fill-rule "evenodd",
+           :d
+           "M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z",
+           :clip-rule "evenodd"}]]]
+       [:button
+        {:type "button",
+         :class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+         :hx-get (str "/products/" (:article-id product))
+         :hx-target "this"
+         :hx-swap "outerHTML"}
+        [:svg
+         {:class "-ml-0.5 mr-1.5 h-5 w-5",
+          :viewBox "0 0 20 20",
+          :fill "currentColor",
+          :aria-hidden "true"}
+         [:path
+          {:fill-rule "evenodd",
+           :d
+           "M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z",
+           :clip-rule "evenodd"}]]
+        "Auswählen"])]
     [:div
      {:class "relative ml-3 sm:hidden"}
      [:button
