a7953abe9db8dbee8e35908e46e5ad20c751cb79
Yunior Alayo <yunior.alayo@gmail.com>
2024-09-10 10:34:28 +0200

    Updated some labels.

--- components/html/src/app/html/product.clj
+++ components/html/src/app/html/product.clj
@@ -131,12 +131,12 @@
           {:class
            "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
            :aria-current "page"}
-          "Products"]
+          "Artikel"]
          [:a
           {:href "#",
            :class
            "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"}
-          "Projects"]
+          "Rechnungen"]
          [:a
           {:href "#",
            :class
