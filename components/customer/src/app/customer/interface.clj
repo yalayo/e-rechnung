3587aba67b341551dfe771472a5f543bce9610bb
Yunior Alayo <yunior.alayo@gmail.com>
2024-09-10 11:34:58 +0200

    Expose function to other components.

--- components/customer/src/app/customer/interface.clj
+++ components/customer/src/app/customer/interface.clj
@@ -1,2 +1,8 @@
 (ns app.customer.interface
   (:require [app.customer.core :as core]))
+
+(defn get-customers []
+  (core/get-customers))
+
+(defn select-customer [session-id customer-id]
+  (core/select-customer session-id customer-id))
