0ec187d17db79bf01ae0dfd0113c8f8af34b9d71
Yunior Alayo <yunior.alayo@gmail.com>
2024-09-10 14:04:16 +0200

    Removed non needed code.

--- components/html/src/app/html/customer.clj
+++ components/html/src/app/html/customer.clj
@@ -291,15 +291,4 @@
      (map customer-data (c/get-customers))]]])
 
 (defn customer-selected [session-id customer-id]
-  (c/select-customer session-id customer-id)
-  [:div {:class "flex justify-center items-center h-9"}
-   [:svg
-    {:class "-ml-0.5 mr-1.5 h-5 w-5",
-     :viewBox "0 0 20 20",
-     :fill "currentColor",
-     :aria-hidden "true"}
-    [:path
-     {:fill-rule "evenodd",
-      :d
-      "M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z",
-      :clip-rule "evenodd"}]]])
+  (c/select-customer session-id customer-id))
